1483233d0d1c7107c91e8c946cc24d55a27d5803
LaloVaz <vazquezgomezeduardo76@gmail.com>
2026-01-14 12:25:00 -0600

    feat: add custom query to LibroRepository for searching by titulo or autor

--- src/main/java/com/example/SistemaBiblioteca/repository/LibroRepository.java
+++ src/main/java/com/example/SistemaBiblioteca/repository/LibroRepository.java
@@ -3,8 +3,11 @@ package com.example.SistemaBiblioteca.repository;
 import com.example.SistemaBiblioteca.model.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LibroRepository extends JpaRepository<Libro,Integer> {
 
-
+    // Busqueda (query) personalizada ya sea con titulo o autor
+    List<Libro> findByTituloIgnoreCaseOrAutorIgnoreCase(String titulo, String autor);
 
 }
