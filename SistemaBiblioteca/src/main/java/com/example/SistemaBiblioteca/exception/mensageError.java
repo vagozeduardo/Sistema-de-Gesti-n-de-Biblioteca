181bc778546ec32960a001fb4c28caf265ead35d
LaloVaz <vazquezgomezeduardo76@gmail.com>
2026-01-15 22:22:47 -0600

    docs: improve comments with swagger annotations.

--- SistemaBiblioteca/src/main/java/com/example/SistemaBiblioteca/exception/mensageError.java
+++ SistemaBiblioteca/src/main/java/com/example/SistemaBiblioteca/exception/mensageError.java
@@ -1,6 +1,7 @@
 package com.example.SistemaBiblioteca.exception;
 
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,8 @@ import org.springframework.http.HttpStatus;
 @NoArgsConstructor
 @AllArgsConstructor
 public class mensageError {
+    @Schema(description = "Variable para guardar el tipo de error o status generado")
     private HttpStatus status;
+    @Schema(description = "mensaje que se desea enviar")
     private String message;
 }
